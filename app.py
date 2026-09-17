9436b3a5f48e1be4cc4c209cd530829aa76032df
Majok-Bol <bolmajok001@gmail.com>
2026-09-17 18:04:32 +0300

    configure db url

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ from flask_jwt_extended import(
     unset_jwt_cookies,
     set_refresh_cookies
 )
+from dotenv import load_dotenv
+import os
+load_dotenv()
 app=Flask(__name__)
+dbs=app.config['DATABASE_URL']=os.getenv("DATABASE_URL")
+print("Database url: ",dbs)
 @app.route("/")
 def hello():
     return 'Hello world'
