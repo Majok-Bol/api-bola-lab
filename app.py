f9d5c5d3eac9602a96e017e1603fa504cca305c2
Majok-Bol <bolmajok001@gmail.com>
2026-09-17 17:58:28 +0300

    install flask-jwt-extended

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("Hello world")
+from flask import Flask,jsonify,request
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import(
+    create_access_token,
+    set_access_cookies,
+    get_jwt_identity,
+    unset_jwt_cookies,
+    set_refresh_cookies
+)
+app=Flask(__name__)
+@app.route("/")
+def hello():
+    return 'Hello world'
+
+
+if __name__=="__main__":
+    app.run(debug=True)
